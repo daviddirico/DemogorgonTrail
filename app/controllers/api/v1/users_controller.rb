250cbe42e1d383d02c68e019b81d7e409b315815
daviddirico <davidjdirico@gmail.com>
2017-12-14 12:19:06 -0500

    refactoring controller

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -7,19 +7,17 @@ class Api::V1::UsersController < ApplicationController
 
   def create
     binding.pry
-    username = params["username"]
-    email = params["email"]
-    encrypted_password = params["encrypted_password"]
-    profile_photo = params["profile_photo"]
-    matching_user = User.where(username: params["username"], email: params["email"])
+    @username = params["username"]
+    @email = params["email"]
+    @encrypted_password = params["encrypted_password"]
+    @profile_photo = params["profile_photo"]
+    matching_user = User.where(username: @username, email: @email)
     if matching_user == []
       user = User.new
-      user.username = params["username"]
-      user.email = params["email"]
-      user.encrypted_password = params["encrypted_password"]
-      binding.pry
-      user.profile_photo = params["profile_photo"]
-      binding.pry
+      user.username = @username
+      user.email = @email
+      user.encrypted_password = @encrypted_password
+      user.profile_photo = @profile_photo
       if user.save
         render json: user
       else
